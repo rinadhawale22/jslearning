80de6d1577bc6385750ef57b4df163407040722f
rinadhawale22 <rinadhawale22@gmail.com>
2023-07-19 11:43:52 +0530

    created js Assignment
    created js Assignment

---
+++ 01_variableAssign.js
@@ -0,0 +1,32 @@
+var myFullName;
+myFullName="Rina dhawale";
+console.log(myFullName);
+
+var myFullName;
+myFullName="Rina  bharat dhawale";
+console.log(myFullName);
+
+var myLove;
+myLove="I love only js";
+console.log(myLove);
+
+var firstName="Rina";
+console.log(firstName);
+
+var lastName="Dhawale";
+console.log(lastName);
+
+var age="23";
+console.log();
+
+var address="smarath colony";
+console.log(address);
+
+var pinCode="444606";
+console.log(pinCode);
+
+var gender="female";
+console.log(gender);
+
+ var married="fales";
+ console.log(married);
---
+++ 03_typeOf.js
@@ -0,0 +1,20 @@
+var bankName;
+bankName="SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+
+var namemark='90%';
+console.log(namemark);
+console.log(typeof namemark);
+
+var working="true";
+console.log(working);
+console.log(typeof working);
+
+var totalCount="Hundred and seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+
+var totalCount='107';
+console.log(totalCount);
+console.log(typeof totalCount);
