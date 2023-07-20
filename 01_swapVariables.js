b1fe0d6b33a70ed540d687feaf28b9672267f36e
rinadhawale22 <rinadhawale22@gmail.com>
2023-07-20 18:14:38 +0530

    created variable swap
    created variable swap

---
+++ 01_swapVariables.js
@@ -0,0 +1,17 @@
+var name1="sweety";
+var name2="cutie";
+console.log('Before swap ==>',"name1:",name1,"name2:",name2);
+var temp=name1;
+name1=name2;
+name2=temp;
+console.log('After swap ==>',"name1:",name1,"name2:",name2);
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log('Before swap ==>','num1:',num1,"num2:",num2,"num3:",num3);
+var temp=num1
+num1=num2;
+num2=num3;
+num3=temp
+console.log('After swap ==>',"num1:",num1,"num2:",num2,"num3:",num3);
