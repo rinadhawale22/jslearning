c4d46e94889c183081737428f545e78c927e012c
rinadhawale22 <rinadhawale22@gmail.com>
2023-08-03 10:34:10 +0530

    assignement is complete
    assignement is complete

---
+++ 08_duplicateElement.js
@@ -0,0 +1,8 @@
+console.log(`removing duplicate elements form the array`);
+let arrayNew1=[4,5,4,5,8,5,7,8,9,7,8];
+for (const element of arrayNew1) {
+    console.log(element);
+    setOfNumbers.add(values);
+}
+console.log(arrayNew1);
+console.log(setOfNumbers);
